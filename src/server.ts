20858acdeac22b22082fefc441d59fc28bb0a767
marubillah57 <marufbillah5157@gmail.com>
2023-11-24 10:21:50 +0600

    mongoose connected with mongodb database

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
 import app from './app';
 import config from './app/config';
+import mongoose from 'mongoose';
 
-app.listen(config.port, () => {
-  console.log(`Server is running on port : ${config.port}`);
-});
+async function server() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log('Mongoose connected');
+
+    app.listen(config.port, () => {
+      console.log(`Server is running on port : ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+server();
